78e424aa2534a352f3c689eb8fc32c480354de73
Danial-Hussain <ali.danial.hussain@gmail.com>
2022-04-01 01:53:22 -0400

    setup next.js + tailwind + typescript starter

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello World!</h1>
+    </div>
+  );
+};
+
+export default Home;
