5c331a08972688ab6db7429e3dc04df104ced79a
Danial-Hussain <ali.danial.hussain@gmail.com>
2022-04-02 19:20:21 -0400

    apostrophes are mean

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -28,14 +28,14 @@ const About: NextPage = () => {
         <div className="text-md tracking-wide text-justify md:text-2xl">
           As a tennis fanatic myself, I&apos;ve always enjoyed following the ATP
           and WTA Grandslam tournaments (Vamos Rafa!). While match schedules and
-          timings were always easy to find, I noticed the same couldn't be said
-          for tournament brackets.
+          timings were always easy to find, I noticed the same couldn&apos;t be
+          said for tournament brackets.
         </div>
         <div className="text-md tracking-wide text-justify md:text-2xl">
           Grandslam Bracket was created to solve this exact problem. You can
           easily relive tennis history through radial brackets spanning from the
           year 2000 to the present, for both ATP and WTA Grandslam tournaments.
-          We'll be adding the latest tournaments as the events take place!
+          We&apos;ll be adding the latest tournaments as the events take place!
         </div>
         <div className="text-md tracking-wide text-justify md:text-2xl">
           If you found this site helpful, consider sharing it with a friend.
