fbabda5e03ebafbbb7b6f6b588ff9fc6ff8e7ead
Danial-Hussain <ali.danial.hussain@gmail.com>
2022-04-03 01:32:12 -0400

    fix styling for mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,11 @@ const Footer = () => {
     <div className="flex flex-col p-4 mt-6 mb-6">
       <div className="w-full h-2 bg-black mb-6"></div>
       <div className="flex flex-row items-center justify-between">
-        <h1 className="text-2xl text-gray-500 font-mono">Grandslam Bracket</h1>
+        <h1 className="text-lg text-gray-500 font-mono md:text-2xl">
+          Grandslam Bracket
+        </h1>
         <div className="flex flex-row justify-center gap-8">
-          <h1 className="text-2xl text-gray-500 text-right font-mono">
+          <h1 className="text-lg text-gray-500 text-right font-mono md:text-2xl">
             Built by Ali Hussain
           </h1>
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,23 +5,23 @@ const Navbar = () => {
     <div className="flex flex-col p-4 mt-6 mb-6">
       <div className="flex flex-col items-center justify-between mb-6 md:flex-row">
         <Link href="/" passHref>
-          <h1 className="text-2xl text-gray-500 font-mono cursor-pointer">
+          <h1 className="text-lg text-gray-500 font-mono cursor-pointer md:text-2xl">
             Grandslam Bracket
           </h1>
         </Link>
         <div className="flex flex-row justify-center gap-8">
           <Link href="/about" passHref>
-            <h1 className="text-2xl text-gray-500 font-mono underline underline-offset-2 decoration-4 decoration-gray-400 cursor-pointer">
+            <h1 className="text-lg text-gray-500 font-mono underline underline-offset-2 decoration-4 decoration-gray-400 cursor-pointer md:text-2xl">
               About
             </h1>
           </Link>
           <Link href="/tournaments" passHref>
-            <h1 className="text-2xl text-gray-500 font-mono underline underline-offset-2 decoration-4 decoration-gray-400 cursor-pointer">
+            <h1 className="text-lg text-gray-500 font-mono underline underline-offset-2 decoration-4 decoration-gray-400 cursor-pointer md:text-2xl">
               Tournaments
             </h1>
           </Link>
           <a
-            className="text-2xl text-gray-500 font-mono underline underline-offset-2 decoration-4 decoration-gray-400 cursor-pointer"
+            className="text-lg text-gray-500 font-mono underline underline-offset-2 decoration-4 decoration-gray-400 cursor-pointer md:text-2xl"
             href="mailto: ali.danial.hussain@gmail.com"
           >
             Contact
