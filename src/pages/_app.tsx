5549f2bf8fb4e1cec0f2fa12f51061137cfb5d97
Danial-Hussain <ali.danial.hussain@gmail.com>
2022-04-03 01:33:22 -0400

    add seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,63 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const title = "Grandslam Bracket";
+  const description =
+    "Relive grandslam tennis history with ATP and WTA tournament brackets dating back to the year 2000!";
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="google-site-verification"
+          content="fpya7Pjh1U76_KZYUbp72SGsMFO1RZ_jTn-Zoo8uY3U"
+        />
+
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta
+          property="og:image"
+          content="https://grandslam.vercel.app/logo.png"
+        />
+
+        <meta
+          name="twitter:image"
+          content="https://grandslam.vercel.app/logo.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="theme-color" content="#000000" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#333333" />
+        <meta name="msapplication-TileColor" content="#333333" />
+        <meta name="theme-color" content="#333333"></meta>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
